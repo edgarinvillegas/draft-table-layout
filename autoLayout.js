b8b1f4cacd43c2bb56fb19b7ad653353e8135ab7
Edgar Villegas Alvarado <edgarinvillegas@hotmail.com>
2019-02-03 18:56:24 -0400

    fix: Fix huge bug in the algorithm

--- autoLayout.js
+++ autoLayout.js
@@ -1,3 +1,8 @@
+/*
+Table autolayout algorithm:
+https://www.w3.org/TR/REC-html40/appendix/notes.html#h-B.5.2
+*/
+
 function autoLayout(tableMatrix, availableTableWidth) {
     const maxReducer = (max, current) =>  current >= max ? current: max ;
     // const maxLengthReducer = (max, current) =>  current.length >= max.length ? current: max ;
@@ -10,7 +15,7 @@ function autoLayout(tableMatrix, availableTableWidth) {
                 // The minimum width is given by the widest text element
                 minWidth: cell.split(' ').map(w => w.length).reduce( maxReducer, 0 ),
                 // The maximum width is given by the widest line
-                maxWidth: cell.split('\n').map(w => w.length).reduce( maxReducer, 0 )
+                maxWidth: cell.split('\n').map(l => l.length).reduce( maxReducer, 0 )
             }
         })
     });
@@ -29,22 +34,39 @@ function autoLayout(tableMatrix, availableTableWidth) {
 }
 
 function getColumnWidthsByMaxMins(columnMaxMinWidths, availableTableWidth) {
-    //Maximum table width is the sum of all the column maximum widths
     const sumReducer = (acum, width) => acum + width;
     //Minimum table width is the sum of all the column minimum widths
     const minTableWidth = columnMaxMinWidths.map(col => col.minWidth).reduce(sumReducer, 0);
-    const W = minTableWidth;
-    const A = availableTableWidth;
-    const D = A - W;
+    //Maximum table width is the sum of all the column maximum widths
+    const maxTableWidth = columnMaxMinWidths.map(col => col.maxWidth).reduce(sumReducer, 0);
+    // 1. The minimum table width is equal to or wider than the available space. In this case, assign the minimum widths and allow the user to scroll horizontally.
+    if(minTableWidth >= availableTableWidth) {
+        console.log('Case 1: Minimum table width is lower than the available. Use minimums')
+        return columnMaxMinWidths.map(col => col.minWidth);
+    }
+    // 2. The maximum table width fits within the available space. In this case, set the columns to their maximum widths.
+    else if(maxTableWidth < availableTableWidth) {
+        console.log('Case 2: Max table width is lower than the available. Use maximum')
+        return columnMaxMinWidths.map(col => col.maxWidth);
+    }
+    // 3. The maximum width of the table is greater than the available space, but the minimum table width is smaller
+    else {
+        console.log('Case 3: Max table width is greater than the available. Calculate')
+        // find the difference between the available space and the minimum table width, lets call it W
+        const W = availableTableWidth - minTableWidth;
+        // Lets also call D the difference between maximum and minimum width of the table.
+        const D = maxTableWidth - minTableWidth;
 
-    const colWidths = columnMaxMinWidths.map( col => {
-        // For each column, let d be the difference between maximum and minimum width of that column
-        const d = col.maxWidth - col.minWidth;
-        // Now set the column's width to the minimum width plus d times W over D
-        return col.minWidth + d * W / D;
-    });
-
-    // Shrink/stretch to fit all available with (step added by me)
-    const totalCalculatedWidth = colWidths.reduce(sumReducer, 0);
-    return colWidths.map(cw => (cw / totalCalculatedWidth) * A);
+        const colWidths = columnMaxMinWidths.map( col => {
+            // For each column, let d be the difference between maximum and minimum width of that column
+            const d = col.maxWidth - col.minWidth;
+            // Now set the column's width to the minimum width plus d times W over D
+            return col.minWidth + d * W / D;
+        });
+        // Shrink/stretch to fit all available with (step added by me)
+        // const totalCalculatedWidth = colWidths.reduce(sumReducer, 0);
+        // return colWidths.map(cw => (cw / totalCalculatedWidth) * A);
+        // return colWidths;
+        return colWidths;
+    }
 }
