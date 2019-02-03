125b74aa1c7e7d2b0715c43d3ea803c4e855ed27
Edgar Villegas Alvarado <edgarinvillegas@hotmail.com>
2019-02-03 03:22:17 -0400

    feat: Improve words randomness

--- getRandomTableMatrix.js
+++ getRandomTableMatrix.js
@@ -1,12 +1,13 @@
 function getRandomTableMatrix(n, m) {
     const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
     const randomWords = lorem.split(' ');
-    function getRandomWords() {
-        //const prob = Math.random();
-        const miu = 5; // 5 words in average
-        const numberOfWords = -Math.log(Math.random())*miu;     // exponential
+    function getRandomExp(miu){
+        return -Math.log(Math.random())*miu;
+    }
+    function getRandomWords(miu) {
+        if(Math.random() < 0.2) return '';
+        const numberOfWords = getRandomExp(miu);     // exponential
         // const numberOfWords = Math.random()*(miu*2);     // uniform
-
         const ret = [];
         for(let i = 0; i < numberOfWords; i++){
             const randomIndex = Math.floor(Math.random()*randomWords.length);
@@ -22,14 +23,14 @@ function getRandomTableMatrix(n, m) {
             () => 'X'.repeat(m).split('')
         );
         for(let j = 0; j < m; j++) {
+            const colAvgWidth = Math.random()*10;
             for(let i = 0; i < n; i++) {
-                const content = getRandomWords();
+                const content = getRandomWords(colAvgWidth);
                 tableMatrix[i][j] = content;
             }
         }
         return tableMatrix;
     }
-
 
     return getTableMatrix();
 }
